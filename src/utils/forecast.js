5fd23e1ff33e1d26b2ab26b4eb4107f5acb73428
maksymprs <max.prs@gmail.com>
2022-10-26 23:25:10 +0300

    More details of forecast added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,6 +1,9 @@
 const { error } = require('console')
 const request = require('request')
 
+
+
+
 const forecast = (latitude, longitude, callback) => {
   const url =`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=03e5dda755c17658357d990788f63ea3&units=metric`
 
@@ -10,7 +13,7 @@ const forecast = (latitude, longitude, callback) => {
     } else if (body.cod != 200) {
       callback('Unable to find location', undefined)
     } else {
-      callback(undefined, `${body.weather[0].description}. It is currently ${body.main.temp} degrees out. It feels like ${body.main.feels_like} degrees out.`)
+      callback(undefined, `${body.weather[0].main}, ${body.weather[0].description}. It is currently ${body.main.temp} degrees out. It feels like ${body.main.feels_like} degrees out. Humidity is ${body.main.humidity}, pressure is ${body.main.pressure}`)
     }
   })
 }
